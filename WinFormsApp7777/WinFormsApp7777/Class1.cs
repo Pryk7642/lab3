24a6cf66c7a4d1b5b1d04e40bbedb70cd185b697
Pryk7642 <110373747+Pryk7642@users.noreply.github.com>
2023-01-15 23:27:47 +0700

    Add files via upload

---
+++ WinFormsApp7777/WinFormsApp7777/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace WinFormsApp7777
+{
+    internal class Class1
+    {
+
+        public string Name
+        {
+            get;set;
+        }
+
+        public string Student_id
+        {
+            get; set;
+        }
+
+        public string Year_of_birth
+        {
+            get; set;
+        }
+
+        public string Height
+        {
+            get; set;
+        }
+
+        public double Grade
+        {
+            get; set;
+        }
+
+        public string Branch
+        {
+            get; set;
+        }
+
+        
+
+        public Class1(string name, string student_id, string year, string height,double grade, string branch) 
+        {
+            Name = name;
+            Student_id = student_id;
+            Year_of_birth = year;
+            Height = height;
+            Grade = grade;
+            Branch = branch;
+
+        }
+    }
+}
---
+++ WinFormsApp7777/WinFormsApp7777/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace WinFormsApp7777
+{
+    public partial class Form1 : Form
+    {
+
+        List<Class1> Class1List = new List<Class1>();
+        List<double> GradeList = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //
+                string name = this.textBox1.Text;
+                string studentid = this.textBox2.Text;
+                string year = this.textBox3.Text;
+                string height = this.textBox4.Text;
+                double grade = double.Parse(this.textBox5.Text);
+                string branch = this.textBox6.Text;
+
+                //
+                Class1 class1 = new Class1(name,studentid,year,height,grade,branch);
+
+                Class1List.Add(class1);
+
+                //
+                BindingSource source = new BindingSource();
+                source.DataSource = Class1List;
+                this.dataGridView1.DataSource= source;
+
+                //
+                foreach (var val in Class1List)
+                {
+                    this.GradeList.Add(val.Grade);
+                }
+                //
+                double Maxgrade = GradeList.Max();
+                double Mingrade = GradeList.Min();
+                //
+                int allClass = Class1List.Count();
+
+                this.labelAllStu.Text = allClass.ToString();
+                this.labelMax.Text = Maxgrade.ToString();
+                this.labelMin.Text = Mingrade.ToString();
+
+
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+
+        }
+    }
+}
